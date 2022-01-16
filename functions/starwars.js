dd15f4d8f40cdebd86947ab6b55de7f935774481
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-01-16 13:09:10 +0530

    Seeting up redis databse on upstash

---
+++ functions/starwars.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+require("dotenv").config();
+const { BASE_API_URL } = require("./utils/constants");
+const Redis = require("ioredis");
+
+const redis = new Redis(process.env.DB_CONNECTION_URL);
+
+exports.handler = async (event, context, callback) => {
+  try {
+    const { name } = JSON.parse(event.body);
+
+    const cachedResult = await redis.get(name);
+    if (cachedResult) {
+      console.log("returning cached data");
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(JSON.parse(cachedResult)),
+      };
+    }
+
+    const { data } = await axios.get(`${BASE_API_URL}/${name}`);
+
+    redis.set(name, JSON.stringify(data.results), "EX", 60);
+
+    console.log("returning fresh data");
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.results),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify("Something went wrong. Try again later."),
+    };
+  }
+};
---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import axios from "axios";
+import Planets from "./components/Planets";
+import People from "./components/People";
+
+const App = () => {
+  const [result, setResult] = useState([]);
+  const [category, setCategory] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const getData = async (event) => {
+    try {
+      const { name } = event.target;
+      setCategory(name);
+      setIsLoading(true);
+      const { data } = await axios({
+        url: "/api/starwars",
+        method: "POST",
+        data: { name },
+      });
+      setResult(data);
+      setErrorMsg("");
+    } catch (error) {
+      setErrorMsg("Something went wrong. Try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div onClick={getData}>
+        <h1>Serverless Redis Demo</h1>
+        <Button variant="info" name="planets" className="action-btn">
+          Planets
+        </Button>
+        <Button variant="info" name="people" className="action-btn">
+          People
+        </Button>
+        {isLoading && <p className="loading">Loading...</p>}
+        {errorMsg && <p className="errorMsg">{errorMsg}</p>}
+        {category === "planets" ? (
+          <Planets planets={result} />
+        ) : (
+          <People people={result} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
